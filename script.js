a4300822507396b49b1669e02ecdb1bb039d243c
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-04-17 16:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function secondHighest(arr){
+
+let max=-1,secMax=-1;
+		for(leti=0;i<n;i++){
+			if(arr[i]>=max){
+				if(arr[i]>max){
+				secMax=max;
+				}
+				max=arr[i];
+			}
+			else if(arr[i]>secMax){
+				secMax=arr[i];
+			}
+		}
+		console.log(secMax);
+}
